7bb354848a26f2dfc65d7e6e682392506aedbd22
Edvin Bryntesson <epost@edvinbryntesson.se>
2024-02-24 17:29:36 +0100

    fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:rusty_chains/main.dart';
+import 'package:rustling_chains/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
