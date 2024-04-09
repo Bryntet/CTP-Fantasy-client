bb7fe4b20875ec829f7fb1d9ee27e49045fd6636
Edvin Bryntesson <epost@edvinbryntesson.se>
2024-04-09 14:12:05 +0200

    make window count down correct amount of hours.

--- lib/tournament.dart
+++ lib/tournament.dart
@@ -580,7 +580,7 @@ class _CountdownBannerState extends State<CountdownBanner> {
   String formatDuration(Duration duration) {
     // Helper function to format Duration to a readable string
     String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final hours = twoDigits(duration.inHours.remainder(24));
+    final hours = twoDigits(duration.inHours);
     final minutes = twoDigits(duration.inMinutes.remainder(60));
     final seconds = twoDigits(duration.inSeconds.remainder(60));
     return "$hours:$minutes:$seconds";
